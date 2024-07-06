d7b84952b703910c3848afcd8045e6e77b490e32
Ali <ali.nezamzadeh@gmail.com>
2024-07-06 19:24:35 +0330

    adding profile page

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -1,8 +1,8 @@
 from flaskblog import app, db, bcrypt
-from flask import render_template, url_for, flash, redirect
-from flaskblog.forms import RegistrationForm, LoginForm
+from flask import render_template, url_for, flash, redirect, request
+from flaskblog.forms import RegistrationForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
-from flask_login import login_user
+from flask_login import login_user, current_user, logout_user, login_required
 
 
 @app.route("/")
@@ -17,6 +17,8 @@ def about():
 
 @app.route('/register/', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
@@ -30,12 +32,38 @@ def register():
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
     form = LoginForm()
     if form.validate_on_submit():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember=form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Email and Password does not match!', 'danger')
     return render_template('login.html', title='Log In', form=form)
+
+
+@app.route("/logout/")
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+
+@app.route("/profile/", methods=['GET', 'POST'])
+@login_required
+def profile():
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Your account has been updated!', 'success')
+        return redirect(url_for('profile'))
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+    image_profile = url_for('static', filename='profile_pics/' + current_user.image_profile)
+    return render_template('profile.html', title="Profile", image_profile=image_profile, form=form)
